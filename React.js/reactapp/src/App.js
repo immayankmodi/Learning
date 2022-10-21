81cdc8a9e96dfa0f271e9a0269c9f4f4a47bb94b
Mayank Modi <modi.mayank89@gmail.com>
2022-10-21 13:55:46 +0530

    added routing using react-router-dom

--- React.js/reactapp/src/App.js
+++ React.js/reactapp/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-//import About from "./componenets/About";
+import About from "./componenets/About";
 import Navbar from "./componenets/Navbar";
 import TextBox from "./componenets/TextBox";
 import React, { useState } from "react";
 import Alert from "./componenets/Alert";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //https://v5.reactrouter.com/web/guides/quick-start
+import Users from "./componenets/Users";
 
 function App() {
   var [mode, setMode] = useState("light");
@@ -33,12 +35,26 @@ function App() {
 
   return (
     <>
-      <Navbar title="Text Utils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextBox heading="Text Utilities" showAlert={showAlert} mode={mode} />
-      </div>
-      {/* <About /> */}
+      <Router>
+        <Navbar title="Text Utils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/users" element={<Users />} />
+            <Route exact
+              path="/"
+              element={
+                <TextBox
+                  heading="Text Utilities"
+                  showAlert={showAlert}
+                  mode={mode}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
